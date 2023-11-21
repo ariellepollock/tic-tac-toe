69d121625e28c82931e11236ca65408ba779c9bb
ariellepollock <ariellecpollock@gmail.com>
2023-11-20 22:39:05 -0600

    Add cached DOM elements., add pseudocode.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 // constants
 //////////////////////////////////
 const colors = {
-    0: 'white',
+    0: 'almond',
     1: 'purple',
     '-1': 'green'
 }
@@ -19,13 +19,20 @@ const colors = {
 //////////////////////////////////
 // state variables
 //////////////////////////////////
-let board
-let turn
-let winner
+let board // an array of 3 nested arrays
+let turn // will be a value of 1 or -1 (1 || -1)
+let winner // null || 1 || -1 || 'T'
 
 //////////////////////////////////
 // cached DOM elements
 //////////////////////////////////
+// grab our HTML elements, save them to variables, and use later
+const messageEl = document.querySelector('h2')
+const playAgainButton = document.querySelector('button')
+// we want to grab our marker elements and save them to an array
+// because we used array brackets, the spread operator is grabbing the items in the nodelist and pushing them into a new array
+const markerEls = [...document.querySelectorAll('#markers > div')]
+console.log('markerEls \n', markerEls)
 
 //////////////////////////////////
 // functions
